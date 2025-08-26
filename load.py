51e7e9651a0c6d4208c0a5072c09309c9dec2ea1
filippa gagern <gagernfilippa@gmail.com>
2025-08-26 09:14:35 +0200

    load model file

--- load.py
+++ load.py
@@ -2,18 +2,18 @@ import pandas as pd
 
 
 def load_data(self):
-        """
-        Load dataset and apply initial preprocessing steps.
-        """
-        df = pd.read_csv(self.filepath)
+    """
+    Load dataset and apply initial preprocessing steps.
+    """
+    df = pd.read_csv(self.filepath)
 
-        # Parse dates and set as index
-        df['date'] = pd.to_datetime(df['date'])
-        df.set_index('date', inplace=True)
+    # Parse dates and set as index
+    df['date'] = pd.to_datetime(df['date'])
+    df.set_index('date', inplace=True)
 
-        # Map binary columns
-        df['holiday'] = df['holiday'].map({'Y': 1, 'N': 0})
-        df['school_day'] = df['school_day'].map({'Y': 1, 'N': 0})
+    # Map binary columns
+    df['holiday'] = df['holiday'].map({'Y': 1, 'N': 0})
+    df['school_day'] = df['school_day'].map({'Y': 1, 'N': 0})
 
-        self.df = df
-        return self.df
+    self.df = df
+    return self.df
